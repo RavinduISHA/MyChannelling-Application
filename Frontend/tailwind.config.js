fc5a62649c7f4c0fd0cd42668235440c67a2fded
Ravindu_Umayanga <rumayanga51@gmail.com>
2023-08-02 19:47:32 +0530

    configure the project

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html","./src/**/*.{js,ts,jsx,tsx}",],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0067FF',
+        yellow: '#FEB60D',
+        purple: '#9771FF',
+        iris: '#01B5C5',
+        heaidng: '#181A1E',
+        text: '#4E545F',
+      },
+      boxShadow: {
+        panelShadow: "rgba(17, 12, 46, 0.15) 0px 48px 100px 0px;"
+      }
+    },
+  },
+  plugins: [],
+}
+
